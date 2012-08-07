4d76e918890e7cc2bb2019e2206ad68a31dc3383
Mark McGuire <mark.b.mcg@gmail.com>
2012-08-07 14:42:10 -0500

    add base collections

--- lobbypy/resources.py
+++ lobbypy/resources.py
@@ -1,3 +1,30 @@
 class Root(object):
+    """
+    Root of lobby system ie monogdb
+    """
     def __init__(self, request):
         self.request = request
+
+class LobbyCollection(object):
+    """
+    Mongodb collection of lobbies
+    """
+    pass
+
+class MatchCollection(object):
+    """
+    Mongodb collection of matches
+    """
+    pass
+
+class PlayerCollection(object):
+    """
+    Mongodb collection of players
+    """
+    pass
+
+class ServerCollection(object):
+    """
+    Mongodb collection of players
+    """
+    pass
