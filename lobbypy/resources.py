7702d8dfbf8fa4efb667f0542c3a7c20f139e4ba
Mark McGuire <mark.b.mcg@gmail.com>
2012-08-07 20:21:36 -0500

    improve resources fix root storing db add basic collection class make collection classes extend base

--- lobbypy/resources.py
+++ lobbypy/resources.py
@@ -3,27 +3,49 @@ class Root(object):
     Root of lobby system ie monogdb
     """
     def __init__(self, request):
-        self.request = request
+        self.db = request.db
 
-class LobbyCollection(object):
+    def __getitem__(self, name):
+        return _assign(self.db[name])
+
+    def __len__(self):
+        return len(self.db)
+
+    def __iter__(self):
+        return iter(self.db)
+
+class Collection(object):
+    def __init__(self, collection):
+        self.collection = collection
+
+    def __getitem__(self, name):
+        raise NotImplementedError
+
+    def __len__(self):
+        return self.collection.count()
+
+    def __iter__(self):
+        raise NotImplementedError
+
+class LobbyCollection(Collection):
     """
     Mongodb collection of lobbies
     """
     pass
 
-class MatchCollection(object):
+class MatchCollection(Collection):
     """
     Mongodb collection of matches
     """
     pass
 
-class PlayerCollection(object):
+class PlayerCollection(Collection):
     """
     Mongodb collection of players
     """
     pass
 
-class ServerCollection(object):
+class ServerCollection(Collection):
     """
     Mongodb collection of players
     """
