b8b814ad657553c6b55fd5b20025829ae325629b
Mark McGuire <mark.b.mcg@gmail.com>
2012-08-08 12:26:36 -0500

    basic login complete remove default template make (very) basic macro for template make view check for _id in session

--- lobbypy/views.py
+++ lobbypy/views.py
@@ -1,11 +1,16 @@
 from pyramid.view import view_config
+from pyramid.renderers import get_renderer
 from pyramid.httpexceptions import HTTPFound
 from lobbypy import resources
 from pyramid_openid.view import process_incoming_request, process_provider_response
 
 @view_config(context=resources.root.Root, renderer='templates/root.pt')
 def root_view(context, request):
-    return {'project':'lobbypy'}
+    player = None
+    if '_id' in request.session:
+        player = context['player'].collection.find_one(request.session['_id'])
+    master = get_renderer('templates/master.pt').implementation()
+    return dict(master=master, player=player)
 
 @view_config(context=resources.root.Root, name='login', renderer='templates/root.pt')
 def login_view(context, request):
