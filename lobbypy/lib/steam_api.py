69ae4b1fe873e8cb66bbebc1bc756075add99780
Mark McGuire <mark.b.mcg@gmail.com>
2012-08-08 16:01:20 -0500

    start steam api work add setting param for api key (VERY SCARY, PULL IT IN DYNAMICALLY) add steam_api functions to lib

---
+++ lobbypy/lib/steam_api.py
@@ -0,0 +1,29 @@
+from pyramid.threadlocal import get_current_registry()
+import logging
+
+log = logging.getLogger(__name__)
+
+def _get_api_key():
+    api_key = get_current_registry().settings['steam.api_key']
+
+def _get_player_summaries_link_base():
+    # TODO: format betterer
+    return 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=' % _get_api_key()
+
+def _get_player_friends_link_base():
+    # TODO: format betterer
+    return 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=%s&steamid=' % _get_api_key()
+
+def get_player_summary(steamid):
+    """
+    Get player data via Steam API GetPlayerSummaries
+    """
+    # TODO: error checking here
+    return json.load(urllib2.urlopen('%s%s' % (_get_player_summaries_link_base(), steamid)))
+
+def get_player_friends(steamid):
+    """
+    Get friend list via Steam API GetFriendList
+    """
+    # TODO: error checking here
+    return  json.load(urllib2.urlopen('%s%s' % (_get_player_friends_link_base(), steamid)))
