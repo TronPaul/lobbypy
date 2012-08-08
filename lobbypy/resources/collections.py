c55ed133cfe2a8dc23fc9e3e3bcdc068541e2816
Mark McGuire <mark.b.mcg@gmail.com>
2012-08-08 13:15:32 -0500

    flesh out Player add base Steam API methods

--- lobbypy/resources/collections.py
+++ lobbypy/resources/collections.py
@@ -56,7 +56,39 @@ class Match(object):
 
 class Player(object):
     def __init__(self, **kwargs):
+        self._id = _id
         self.steamid = steamid
+    
+    def __getattr__(self, name):
+        if name == 'name':
+            # Player name from steam
+            return self._get_persona_name()
+        raise AttributeError
+
+    def _get_persona_name(self):
+        return self._get_player_summary()['personaname']
+
+    def _get_friends(self):
+        return self._get_friend_list()
+
+    def _get_avatar_url(self, size='small'):
+        summary = self._get_player_summary()
+        if size == 'large':
+            return summary['avatarfull']
+        elif size == 'medium':
+            return summary['avatarmedium']
+        else:
+            return summary['avatar']
+
+    # TODO: cache this
+    def _get_player_summary(self):
+        # Do Steam API call to get all data from GetPlayerSummaries for steamid
+        pass 
+
+    # TODO: cache this
+    def _get_friend_list(self):
+        # Do Steam API call to get all data from GetFriendList for steamid
+        pass
 
 class Server(object):
     pass
