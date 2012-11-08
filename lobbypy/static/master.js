2fbe30fb3cf2de618f6cecb30b205e53d741e482
Mark McGuire <mark.b.mcg@gmail.com>
2012-11-08 07:25:34 +0000

    Improve site frontent - many changes (should really use atomic commits

--- lobbypy/static/master.js
+++ lobbypy/static/master.js
@@ -12,18 +12,62 @@ $(document).ready(function() {
     });
 
     var LobbyView = Backbone.View.extend({
+        events: {
+            "click .team-title": "set_team",
+            "click .spectator-title": "set_spectator",
+            "click #leave": "leave",
+        },
+
+        set_team: function(evt) {
+            evt.preventDefault();
+
+            team_id = $(evt.currentTarget).parent().attr("id").slice(-1);
+            lobby_s.emit('set_team', team_id);
+        },
+
+        set_spectator: function(evt) {
+            evt.preventDefault();
+            lobby_s.emit('set_team', undefined);
+        },
+
+        leave: function(evt) {
+            evt.preventDefault();
+            lobby_s.emit('leave');
+            this.undelegateEvents();
+            window.location.hash = "#";
+        },
+
         initialize: function() {
-            // rerender on change
-            this.model.bind('change', this.render);
+            var me = this;
+            lobby_s.on('destroy', function() {
+                me.undelegateEvents();
+                window.location.hash = '#';
+            });
+
+            lobby_s.on('leave', function() {
+                me.undelegateEvents();
+                window.location.hash = '#';
+            });
+
+            lobby_s.on('update', function(lobby) {
+                me.model = lobby;
+                me.render();
+            });
         },
 
         render: function() {
+            var me = this;
+
             var template = Handlebars.compile($("#lobby_template").html());
 
-            this.$el.html(template(this.model.toJSON()));
+            $.each(this.model.teams, function(index, value) {
+                value.id = index;
+            });
+
+            $(this.el).html(template(this.model));
 
             return this;
-        }
+        },
     });
 
     var LobbiesCollection = Backbone.Collection.extend({
@@ -36,9 +80,11 @@ $(document).ready(function() {
         },
 
         create_lobby: function(evt) {
-            evt.preventDefault()
+            evt.preventDefault();
+            var me = this;
 
             var lobby_name = $('#lobby_name').val();
+            var _hidden_events = {};
 
             // do ajax create of lobby
             $.ajax({
@@ -46,10 +92,18 @@ $(document).ready(function() {
                 data: {
                     name: lobby_name
                 },
-                error: function(jqXHR) {
+                beforeSend: function() {
+                    _hidden_events = lobbies_s.$events;
+                    lobbies_s.$events = {};
                 },
-                success: function(data, textStatus) {
+                success: function(lobby_id) {
+                    lobbies_s.emit('unsubscribe');
+                    me.undelegateEvents();
+                    window.location.hash = '#lobby/' + lobby_id;
                 },
+                error: function() {
+                    lobbies_s.$events = _hidden_events;
+                }
             });
 
             $("#lobby_name").val("");
@@ -60,23 +114,26 @@ $(document).ready(function() {
 
             // recieve create signal from server
             lobbies_s.on('create', function(lobby) {
-                var lobby_item = new LobbyView({
-                    model: new LobbyModel({
-                        lobby: lobby
-                    })
-                });
-
-                // render it to the DOM
-                el = lobby_item.render().el;
-                me.$("#lobbies").append(el);
+                me.collection.unshift(lobby);
+                me.render();
             });
 
             // recieve destroy signal from server
             lobbies_s.on('destroy', function(lobby_id) {
+                lobby_old = me.collection.filter(function(lobby) {
+                    lobby.id === lobby_id
+                })[0];
+                me.collection.pop(lobby_old);
+                me.render();
             });
 
             // recieve lobby update
-            lobbies_s.on('update', function(lobby_id, lobby_info) {
+            lobbies_s.on('update', function(lobby) {
+                lobby_old = me.collection.filter(function(lobby_old) {
+                    lobby_old.id === lobby.id;
+                })[0];
+                lobby_old = lobby;
+                me.render();
             });
         },
 
@@ -85,7 +142,7 @@ $(document).ready(function() {
 
             var template = Handlebars.compile($("#lobbies_template").html());
 
-            $(this.el).html(template());
+            $(this.el).html(template({lobbies: this.collection}));
 
             return this;
         },
@@ -100,28 +157,44 @@ $(document).ready(function() {
         },
 
         index: function() {
+            var view = new LobbiesView({
+                el: $("#container"),
+                collection: [],
+            });
+            view.render();
             // get lobbies via ajax
             $.ajax({
                 url: '/_ajax/lobby/all',
-                success: function(data) {
+                success: function(lobbies) {
+                    view.collection = lobbies;
+                    view.render();
                 },
             });
             // connect to the websocket
             lobbies_s.emit('subscribe');
-            var view = new LobbiesView({
-                el: $("#container"),
-            });
-
-            view.render();
         },
 
         // View a lobby
         lobby: function(lobby_id) {
-            alert('Viewing lobby' + lobby_id);
+            var view = new LobbyView({
+                el: $("#container"),
+                model: {},
+            });
+
+            // get lobby via ajax
+            $.ajax({
+                url: '/_ajax/lobby/' + lobby_id,
+                success: function(lobby) {
+                    view.model = lobby;
+                    view.render();
+                    lobbies_s.emit('unsubscribe');
+                    lobby_s.emit('join', lobby_id);
+                },
+            });
         },
     });
 
     // start backbone routing
     var app = new Router();
-    Backbone.history.start({ pushState: true });
+    Backbone.history.start();
 });
